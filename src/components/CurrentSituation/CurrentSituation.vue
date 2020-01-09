e51451c0e74c604dd4a1e4711532ced2d90574db
Adhshran Kirubakaran <adhshran.kirubakaran@swisscom.com>
2020-01-09 08:04:52 +0100

    Jumbotron aufgeteilt

--- src/components/CurrentSituation/CurrentSituation.vue
+++ src/components/CurrentSituation/CurrentSituation.vue
@@ -5,15 +5,16 @@
         <h1 class="display-4">Monetarisierung früher</h1>
         <p class="lead"></p>
       </div>
-    </div>
-    <div class="jumbotron jumbotron-fluid" id="heute">
-      <div class="container">
+      <div class="container" id="heute">
         <h1 class="display-4">Monetarisierung heute</h1>
         <p class="lead">
           Videospiele sind heutzutage so angesagt wie wahrscheinlich nie zuvor. Videospiele werden entweder am Computer oder direkt an der Spielkonsole gespielt. Der Kunde kauft sich hierbei das Videospiel und kann dieses auch spielen. Heute können aber bei vielen Spielen weitere Spielinhalte zusätzlich im Spiel selber gekauft werden. Diese Käufe im Spiel sind teilweise nötig um überhaupt gegen die Gegner konkurrenzfähig zu sein.
-          <br /> <br>Wir erklären dies anhand eines Beispiels. Eine Person kauf sich das neue Fifa 20. Wie jeder Spieler, welcher sich Fifa 20 leistet, will auch diese Person den Spielmodus FUT spielen. FUT ist ein Spielmodus, in dem der Spieler sich selber ein Team zusammenstellen kann und Online gegen andere Spieler spielen kann, um sich hierbei ein konkurrenzfähiges Team zusammenstellen zu können, kann der Spieler sich sogenannte Packs kaufen.
-          <br /><br>In diesen Packs befinden sich Spieler, jedoch weiss man vor dem Öffnen der Packs nicht welche Spieler sich dort befinden. Solche Packs kosten bis zu 25.- Franken. Doch um wirklich einmal einen guten Spieler zu ziehen, müssen mehrere solche Packs gekauft werden. Doch nun hat der Spieler folgendes Problem: Entschliesst sich der Spieler nun keine Packs zu kaufen, wird dieser Spieler gegen viel bessere Teams treffen und er ist nicht konkurrenzfähig. Es gewinnt nicht der bessere Spieler, sondern der Spieler, welcher mehr Geld in das Spiel investiert hat. Grob zusammengefasst entsteht durch diese Art von Videospielen ein unfairer Wettbewerb.
-          <br /><br>Fifa ist hierbei nur ein Beispiel von vielen. Bei solchen zusätzlichen Einkäufen neben dem Videospiel spricht man von sogenannten In-Game-Käufen. Diese In-Game-Käufe sind in der heutigen Zeit gang und gäbe. Die Spielhersteller erzielen hierbei zusätzlich sehr viel Geld. Dies erkennt man in der untenstehenden Tabelle. Es gibt einen ständigen Anstieg an In-Game-Käufen.
+          <br />
+          <br />Wir erklären dies anhand eines Beispiels. Eine Person kauf sich das neue Fifa 20. Wie jeder Spieler, welcher sich Fifa 20 leistet, will auch diese Person den Spielmodus FUT spielen. FUT ist ein Spielmodus, in dem der Spieler sich selber ein Team zusammenstellen kann und Online gegen andere Spieler spielen kann, um sich hierbei ein konkurrenzfähiges Team zusammenstellen zu können, kann der Spieler sich sogenannte Packs kaufen.
+          <br />
+          <br />In diesen Packs befinden sich Spieler, jedoch weiss man vor dem Öffnen der Packs nicht welche Spieler sich dort befinden. Solche Packs kosten bis zu 25.- Franken. Doch um wirklich einmal einen guten Spieler zu ziehen, müssen mehrere solche Packs gekauft werden. Doch nun hat der Spieler folgendes Problem: Entschliesst sich der Spieler nun keine Packs zu kaufen, wird dieser Spieler gegen viel bessere Teams treffen und er ist nicht konkurrenzfähig. Es gewinnt nicht der bessere Spieler, sondern der Spieler, welcher mehr Geld in das Spiel investiert hat. Grob zusammengefasst entsteht durch diese Art von Videospielen ein unfairer Wettbewerb.
+          <br />
+          <br />Fifa ist hierbei nur ein Beispiel von vielen. Bei solchen zusätzlichen Einkäufen neben dem Videospiel spricht man von sogenannten In-Game-Käufen. Diese In-Game-Käufe sind in der heutigen Zeit gang und gäbe. Die Spielhersteller erzielen hierbei zusätzlich sehr viel Geld. Dies erkennt man in der untenstehenden Tabelle. Es gibt einen ständigen Anstieg an In-Game-Käufen.
         </p>
       </div>
     </div>
